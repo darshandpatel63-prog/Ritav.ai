64bcbad401025191ff72ae0167a3e471afe61189
D D Patel <darshandpatel63@gmail.com>
2026-09-24 10:17:53 +0530

    Configure Keychain cinterop header search directory

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -9,6 +9,7 @@ kotlin {
             cinterops {
                 val ritavKeychain by creating {
                     definitionFile.set(project.file("src/nativeInterop/cinterop/ritav_keychain.def"))
+                includeDirs.allHeaders("src/nativeInterop/cinterop")
                 }
             }
         }
